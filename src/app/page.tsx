5ca69eb77261bc6c6e43ddb350fa2d3af1058ebd
NiceTwice <onlynicetwice@gmail.com>
2023-07-20 04:13:22 -0300

    refactor(main page):GamesList component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Button from '@/components/Button'
-import { PiWalletFill } from 'react-icons/pi'
-import Image from 'next/image'
+import { GamesList } from './components/GamesList'
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 min-h-screen items-center justify-center bg-gradient-to-t from-rose-900 to-violet-900 font-poppins">
-      <Button color="accent" variant='outlined'>
-        Button
-        <PiWalletFill size={25} />
-      </Button>
+    <main className="flex flex-col py-24 min-h-screen w-full items-center justify-center bg-zinc-800">
+      <GamesList/>
     </main>
   )
 }
