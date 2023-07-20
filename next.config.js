174eba28c4c9f1d182fac9f69127be12caf370fb
NiceTwice <onlynicetwice@gmail.com>
2023-07-20 04:15:16 -0300

    config(global): .env variables and minors configurations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:["cdn.wcbackoffice.com"]
+    }
+}
 
 module.exports = nextConfig
